f1f45fae40b9e1c2f816b1d6c66747e22be5c25f
Aayush Singla <4n0nym0us1147@gmail.com>
2023-11-07 16:45:22 +0530

    11/7/2023

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=100, default="")
+    product_price = models.FloatField(default=-1)
+
+    product_image = models.ImageField(upload_to="Shop/images", default="")
+
+    product_category = models.CharField(max_length=50, default="")
+    product_subCategory = models.CharField(max_length=50, default="")
+    product_description = models.CharField(max_length=1000, default="")
+
+    product_publishDate = models.DateField()
+
+    def __str__(self):
+        return self.product_name
--- Shop/views.py
+++ Shop/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'Shop/index.html')
+    products = list(Product.objects.all())
+
+    n = len(products) // 4 if len(products) % 4 == 0 else len(products) // 4 + 1
+    idx = list(range(n))
+
+    productsLi = []
+    if n > 1:
+        tempLi = []
+        for i in range(len(products)):
+            tempLi.append(products[i])
+            if i == len(products)-1:
+                productsLi.append(tempLi)
+                tempLi = []
+            elif len(tempLi) >= 4:
+                productsLi.append(tempLi)
+                tempLi = []
+    else:
+        productsLi.append(products)
+
+    slides = zip(idx, productsLi)
+    slides = list(item for item in slides)
+
+    return render(request, 'Shop/index.html',
+                  {'slides': slides,
+                   'buttons': idx}
+                  )
+
+    # return render(request, 'Shop/index_old.html')
 
 
 def about(request):
@@ -27,4 +56,4 @@ def product(request):
 
 
 def checkout(request):
-    pass
+    return render(request, 'Shop/checkout.html')

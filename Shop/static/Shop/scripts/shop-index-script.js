ca6a0384a2e9d717b28cfa357b20ec0610f93b1f
Aayush Singla <4n0nym0us1147@gmail.com>
2023-11-09 15:04:00 +0530

    Refined The Navbar and applied uniform styles to the entire website

--- Shop/static/Shop/scripts/shop-index-script.js
+++ Shop/static/Shop/scripts/shop-index-script.js
@@ -1,14 +1,3 @@
-if (localStorage.getItem('cart') == null) {
-    var cart = {}
-} else {
-    var cart = JSON.parse(localStorage.getItem('cart'))
-    var items = Object.keys(cart).length
-    if (items > 9) {
-        items = '9+'
-    }
-    document.getElementById('cart-count').innerHTML = items
-}
-
 $('.cart').click(function () {
     var idstr = this.id.toString()
     if (cart[idstr] != undefined) {

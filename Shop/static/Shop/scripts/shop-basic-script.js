f1d80e3e94258d9129a582851b068967b65d1775
Aayush Singla <4n0nym0us1147@gmail.com>
2023-11-14 17:52:06 +0530

    Saving the work for the day (11/14/2023)

--- Shop/static/Shop/scripts/shop-basic-script.js
+++ Shop/static/Shop/scripts/shop-basic-script.js
@@ -13,3 +13,45 @@ if (items > 9) {
 }
 
 document.getElementById('cart-count').innerHTML = items;
+
+const updateCartOffCanvas = () => {
+    console.log('updating..')
+    dropdownBtn = document.getElementById('cart-offcanvas-dropdownMenuButton')
+
+    var noItems = false;
+    if (Object.keys(cart).length == 0) {
+        offCanvasHeading = "You have no items in your cart. Add some to get started.";
+        dropdownBtn.classList.add('disabled')
+        console.log('adding..')
+        noItems = true;
+    } else {
+        offCanvasHeading = "Items in your Cart:"
+        dropdownBtn.classList.remove('disabled')
+        console.log('removin..')
+    }
+
+    console.log(dropdownBtn.classList)
+
+    document.getElementById('cart-offcanvas-title').innerHTML = offCanvasHeading;
+
+    content = "<hr>"
+
+    var i = 1;
+    for (const[item, count] of Object.entries(cart)) {
+        cnt = `<div class="row cart-item-row">`;
+        cnt = cnt + `<div class="sr-no">${i}</div>`
+        cnt = cnt + `<img src="..." alt="..." class="prd-img"></img>`
+        cnt = cnt + `<div class="cart-desc">
+                      Name: ${item}
+                     </div>`
+
+
+        cnt = cnt + `</div>`
+        content = content + cnt;
+    }
+
+    console.log(noItems)
+    document.getElementById('cart-offcanvas-body').innerHTML = content;
+}
+
+setInterval(updateCartOffCanvas(), 100)
